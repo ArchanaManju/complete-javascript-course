213577136deff3c3803dcfe6cb68c26f23788735
ArchanaManju <archanahm2021@gmail.com>
2022-01-20 13:26:00 +0000

    created my first game

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,10 @@
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
+// console.log("archana");
+// console.log(23);
+// let firstName = "archana"
+// console.log(firstName)
+
+
+true;
+console.log(true)
